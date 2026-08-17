bbb00eea0eed3b4bd0a8536e4073031a6dc4f321
DansUkVlogs <64607189+DansUkVlogs@users.noreply.github.com>
2026-08-17 03:19:04 +0100

    Configure Wear OS app

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "uk.dansvlogs.clinicalwatch"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "uk.dansvlogs.clinicalwatch"
+        minSdk = 30
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.wear:wear:1.3.0")
+}
